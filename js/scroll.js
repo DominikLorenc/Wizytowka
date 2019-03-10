cfbbe53d7060107468a4aeb9781342d522890d85
Dominik <dominiklorenc1270@gmail.com>
2019-03-10 03:25:18 +0100

    Footer
    Add footer and link to github , facebook and likedin

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+$(document).on("scroll", function() {
+  const windowHeight = $(window).height();
+  const scrollValue = $(this).scrollTop();
+  //   console.log(scrollValue);
+  const section = $(".skills-container");
+  const sectionFromTop = section.offset().top;
+  console.log(sectionFromTop);
+  const sectionHight = section.outerHeight();
+  const skillLevel = $(".skill-level");
+
+  if (scrollValue > sectionFromTop + sectionHight - windowHeight) {
+    skillLevel.addClass("on");
+  }
+});
